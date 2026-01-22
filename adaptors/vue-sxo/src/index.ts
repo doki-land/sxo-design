f1f3cce10451bd5baba7420e4e93968e768c634c
aster <192608617@qq.com>
2026-01-22 11:34:43 +0800

    refactor(vue-sxo): 统一组件导出命名添加Sxo前缀
    将所有导出的Vue组件添加Sxo前缀以保持命名一致性，避免潜在的命名冲突

--- adaptors/vue-sxo/src/index.ts
+++ adaptors/vue-sxo/src/index.ts
@@ -1,65 +1,64 @@
-export * from './components/Accordion';
+export { Accordion as SxoAccordion } from './components/Accordion';
 export {
-    AdminShell,
-    DescriptionList,
-    Kanban,
-    LoginScreen,
-    PageHeader,
-    QueryFilter,
-    StatCard,
-    StepForm,
+    AdminShell as SxoAdminShell,
+    DescriptionList as SxoDescriptionList,
+    Kanban as SxoKanban,
+    LoginScreen as SxoLoginScreen,
+    PageHeader as SxoPageHeader,
+    QueryFilter as SxoQueryFilter,
+    StatCard as SxoStatCard,
+    StepForm as SxoStepForm,
 } from './components/Admin';
-export * from './components/Alert';
-export * from './components/Avatar';
-export * from './components/BackTop';
-export * from './components/Badge';
-export * from './components/Box';
-export { Breadcrumb, BreadcrumbItem } from './components/Breadcrumb';
-export * from './components/Button';
-export * from './components/Calendar';
-export * from './components/Card';
-export * from './components/Cascader';
-export { Checkbox, CheckboxGroup } from './components/Checkbox';
-export * from './components/CommandPalette';
-export * from './components/DatePicker';
-export { Descriptions, DescriptionsItem } from './components/Descriptions';
-export * from './components/Dialog';
-export * from './components/Divider';
-export * from './components/Drawer';
-export { Dropdown, DropdownItem } from './components/Dropdown';
-export * from './components/Empty';
-export { Progress, Skeleton, Spinner } from './components/Feedback';
-export { Form, FormItem } from './components/Form';
-export { I18nProvider, useI18n } from './components/I18n';
-export * from './components/Icon';
-export * from './components/Input';
-export * from './components/Layout';
-export * from './components/Mentions';
-export * from './components/Menu';
-export * from './components/Pagination';
-export * from './components/Popconfirm';
-export * from './components/Popover';
-export * from './components/Radio';
-export * from './components/Rate';
-export * from './components/Result';
-export * from './components/Search';
-export * from './components/Select';
-export * from './components/Skeleton';
-export * from './components/Slider';
-export * from './components/Statistic';
-export { Step, Steps } from './components/Steps';
-export * from './components/Switch';
-export * from './components/Table';
-export * from './components/Tabs';
-export * from './components/Tag';
-export { Timeline, TimelineItem } from './components/Timeline';
-export * from './components/Toast';
-export { ToastProvider, useToast } from './components/Toast';
-export * from './components/Tooltip';
-export * from './components/Transfer';
-export * from './components/Tree';
-export * from './components/TreeSelect';
-export * from './components/Upload';
-export * from './components/VirtualList';
+export { Alert as SxoAlert } from './components/Alert';
+export { Avatar as SxoAvatar } from './components/Avatar';
+export { BackTop as SxoBackTop } from './components/BackTop';
+export { Badge as SxoBadge } from './components/Badge';
+export { Box as SxoBox } from './components/Box';
+export { Breadcrumb as SxoBreadcrumb, BreadcrumbItem as SxoBreadcrumbItem } from './components/Breadcrumb';
+export { Button as SxoButton } from './components/Button';
+export { Calendar as SxoCalendar } from './components/Calendar';
+export { Card as SxoCard } from './components/Card';
+export { Cascader as SxoCascader } from './components/Cascader';
+export { Checkbox as SxoCheckbox, CheckboxGroup as SxoCheckboxGroup } from './components/Checkbox';
+export { CommandPalette as SxoCommandPalette } from './components/CommandPalette';
+export { DatePicker as SxoDatePicker } from './components/DatePicker';
+export { Descriptions as SxoDescriptions, DescriptionsItem as SxoDescriptionsItem } from './components/Descriptions';
+export { Dialog as SxoDialog } from './components/Dialog';
+export { Divider as SxoDivider } from './components/Divider';
+export { Drawer as SxoDrawer } from './components/Drawer';
+export { Dropdown as SxoDropdown, DropdownItem as SxoDropdownItem } from './components/Dropdown';
+export { Empty as SxoEmpty } from './components/Empty';
+export { Progress as SxoProgress, Skeleton as SxoSkeleton, Spinner as SxoSpinner } from './components/Feedback';
+export { Form as SxoForm, FormItem as SxoFormItem } from './components/Form';
+export { I18nProvider as SxoI18nProvider, useI18n } from './components/I18n';
+export { Icon as SxoIcon } from './components/Icon';
+export { Input as SxoInput } from './components/Input';
+export { Stack as SxoStack, Grid as SxoGrid, Container as SxoContainer } from './components/Layout';
+export { Mentions as SxoMentions } from './components/Mentions';
+export { Menu as SxoMenu } from './components/Menu';
+export { Pagination as SxoPagination } from './components/Pagination';
+export { Popconfirm as SxoPopconfirm } from './components/Popconfirm';
+export { Popover as SxoPopover } from './components/Popover';
+export { Radio as SxoRadio } from './components/Radio';
+export { Rate as SxoRate } from './components/Rate';
+export { Result as SxoResult } from './components/Result';
+export { Search as SxoSearch } from './components/Search';
+export { Select as SxoSelect } from './components/Select';
+export { Skeleton as SxoSkeletonBase } from './components/Skeleton';
+export { Slider as SxoSlider } from './components/Slider';
+export { Statistic as SxoStatistic } from './components/Statistic';
+export { Step as SxoStep, Steps as SxoSteps } from './components/Steps';
+export { Switch as SxoSwitch } from './components/Switch';
+export { Table as SxoTable } from './components/Table';
+export { Tabs as SxoTabs } from './components/Tabs';
+export { Tag as SxoTag } from './components/Tag';
+export { Timeline as SxoTimeline, TimelineItem as SxoTimelineItem } from './components/Timeline';
+export { ToastProvider as SxoToastProvider, useToast } from './components/Toast';
+export { Tooltip as SxoTooltip } from './components/Tooltip';
+export { Transfer as SxoTransfer } from './components/Transfer';
+export { Tree as SxoTree } from './components/Tree';
+export { TreeSelect as SxoTreeSelect } from './components/TreeSelect';
+export { Upload as SxoUpload } from './components/Upload';
+export { VirtualList as SxoVirtualList } from './components/VirtualList';
 export * from './hooks';
 export * from './plugin';
